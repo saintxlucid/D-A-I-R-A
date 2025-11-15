e4a945f108e4dffe00b3872debd4bb78760aa26d
saintxlucid <lucid.saintsaintsaint@gmail.com>
2025-11-15 18:54:59 +0200

    Auto-commit: 2025-11-15 18:54:59

---
+++ packages/backend/src/auth/dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
+
+export class VerifyEmailDto {
+  @IsString()
+  token: string;
+}
